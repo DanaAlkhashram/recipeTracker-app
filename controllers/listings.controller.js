df9b3e576d048a3cea62682f8ff3fe899fe89774
Dana <danalkhashram@gmail.com>
2025-07-22 09:41:14 +0300

    update listing in database and starting comments feature

--- controllers/listings.controller.js
+++ controllers/listings.controller.js
@@ -24,8 +24,8 @@ router.post('/', isSignedIn, async (req, res) => {
 // VIEW ALL LISTINGS
 router.get('/', async (req, res) => {
     try {
-        const foundrecipes = await Recipe.find()
-        res.render('recipes/index.ejs', { foundrecipes: foundrecipes })
+        const foundrecipe = await Recipe.find()
+        res.render('recipes/index.ejs', { foundrecipe: foundrecipe })
     } catch (err) {
         console.log(err)
         res.send('Something went wrong')
@@ -35,8 +35,8 @@ router.get('/', async (req, res) => {
 // VIEW A SINGLE LISTING
 router.get('/:recipeId', async (req, res) => {
     try {
-        const foundrecipes = await Recipe.findById(req.params.recipeId).populate('chef')
-        res.render('recipes/show.ejs', { foundrecipes })
+        const foundrecipe = await Recipe.findById(req.params.recipeId).populate('chef')
+        res.render('recipes/show.ejs', { foundrecipe })
     } catch (error) {
         console.log(error)
         res.redirect('/recipes')
@@ -45,10 +45,10 @@ router.get('/:recipeId', async (req, res) => {
 
 // DELETE LISTING FROM DATABASE
 router.delete('/:recipeId', isSignedIn, async (req, res) => {
-    const foundrecipes = await Recipe.findById(req.params.recipeId).populate('chef')
+    const foundrecipe = await Recipe.findById(req.params.recipeId).populate('chef')
 
-    if (foundrecipes.chef._id.equals(req.session.user._id)) {
-        await foundrecipes.deleteOne()
+    if (foundrecipe.chef._id.equals(req.session.user._id)) {
+        await foundrecipe.deleteOne()
         return res.redirect('/recipes')
     }
     return res.send('Not authorized')
@@ -57,10 +57,10 @@ router.delete('/:recipeId', isSignedIn, async (req, res) => {
 // RENDER THE EDIT FORM VIEW
 router.get('/:recipeId/edit', isSignedIn, async (req, res) => {
     try {
-        const foundrecipes = await Recipe.findById(req.params.recipeId).populate('chef')
+        const foundrecipe = await Recipe.findById(req.params.recipeId).populate('chef')
 
-        if (foundrecipes.chef._id.equals(req.session.user._id)) {
-            res.render('recipes/edit.ejs', { foundrecipes: foundrecipes })
+        if (foundrecipe.chef._id.equals(req.session.user._id)) {
+            res.render('recipes/edit.ejs', { foundrecipe: foundrecipe })
         }
     } catch (err) {
         console.log(err)
@@ -69,6 +69,30 @@ router.get('/:recipeId/edit', isSignedIn, async (req, res) => {
 
 })
 
+// HANDLE EDIT FORM SUBMISSION 
+router.put('/:recipeId', isSignedIn, async (req, res) => {
+    try {
+        const foundrecipe = await Recipe.findById(req.params.recipeId).populate('chef')
+
+        if (foundrecipe.chef._id.equals(req.session.user._id)) {
+            await Recipe.findByIdAndUpdate(req.params.recipeId, req.body, { new: true })
+            return res.redirect(`/recipes/${req.params.recipeId}`)
+        }
+    } catch (err) {
+        console.log(err)
+        return res.send('Not authorized')
+    }
+})
+
+// POST COMMENT FORM TO THE DATABASE
+router.post('/:recipeId/comments', isSignedIn, async (req, res) => {
+    const foundrecipe = await Recipe.findById(req.params.recipeId)
+    req.body.author = req.session.user._id
+    // console.log(req.body)
+    foundrecipe.comments.push(req.body)
+    await foundrecipe.save()
+    res.redirect(`/recipes/${req.params.recipeId}`)
+})
 
 
 module.exports = router
--- models/listing.js
+++ models/listing.js
@@ -1,7 +1,16 @@
 const mongoose = require('mongoose')
 const Schema = mongoose.Schema
 
-const recipelSchema = new Schema({
+
+const commentSchema = new mongoose.Schema({
+    comment:String,
+    authOr: {
+        type: Schema.Types.ObjectId,
+        ref: 'User'
+    }
+}, { timestamps: true })
+
+const recipelSchema = new mongoose.Schema({
     title: String,
     ingredients: String,
     steps: String,
@@ -9,7 +18,8 @@ const recipelSchema = new Schema({
     chef: {
         type: Schema.Types.ObjectId,
         ref: 'User'
-    }
+    },
+    comments: [commentSchema]
 }, { timestamps: true })
 
 module.exports = mongoose.model('Recipe', recipelSchema)
